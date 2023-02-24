d3a16b4af7ca34956f0c55baba52acaa9c6c462d
4rkhamknight01 <74088395+4rkhamknight01@users.noreply.github.com>
2023-02-24 21:40:07 +0530

    Update hash.py

--- hash.py
+++ hash.py
@@ -6,9 +6,11 @@ pln_text = CryptContext(schemes=["bcrypt"], deprecated = "auto")
 class Hash():
     def encrypt(password:str):
         return pln_text.hash(password)
+    #this function encrypts the password provided provided by the user and stores it in the database alongside the username
     
     def verify(password_hash, password):
         return pln_text.verify(password, password_hash)
+    #this function checks/verifies whether the entered password has the same hash value as the on epresent in the databse.
 
 
 

9d905190963d5323c695e7d2034b1d65dad08159
4rkhamknight01 <74088395+4rkhamknight01@users.noreply.github.com>
2023-02-24 21:50:33 +0530

    Update oauth.py

--- oauth.py
+++ oauth.py
@@ -4,6 +4,7 @@ from fastapi.security import OAuth2PasswordBearer
 import status
 
 oauth2scheme = OAuth2PasswordBearer(tokenUrl="login")
+# we are using OAuth2PasswordBearer to validate the current logged in user.
 
 #function to find the current user that generated the token
 def get_current_user(token:str = Depends(oauth2scheme)):
@@ -13,5 +14,6 @@ def get_current_user(token:str = Depends(oauth2scheme)):
         headers={"WWW-Authenticate": "Bearer"},
     )
     return verify_token(token,credentials_exception)
+#this functions finds the current user that generated the token when logging in.
 
 
